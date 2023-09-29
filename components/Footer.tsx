ca40a32b019a9193f1cc55d130bb24bc8b4fde6c
Mogens Winther <mw@mowin.dk>
2023-09-29 18:29:26 +0200

    Updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,28 @@
-import { LNXRow } from "../libs/lib-lnx/components"
+import Link from "next/link";
+import { LNXLinkText, LNXRow } from "../libs/lib-lnx/components"
 
 export default function Footer({ ...props }) {
     return (
         <div className='grid grid-flow-row bg-dark text-light text-center shadow-lg mt-16 pb-16'>
-            <LNXRow cols='3'>
+            <LNXRow cols='2'>
                 <div className="w-56">
-                    Contact:
-                    mw@linuxconsulting.dk
+                    <div className="font-bold">Contact</div>
+                    <LNXLinkText href='/get-in-touch' text='Get in touch' className='block' />
+                    <LNXLinkText href='https://www.linkedin.com/in/mogenswinther/' text='LinkedIn' className='block' />
+                </div>
+
+                <div className="w-56 items-stretch">
+                    <div className="font-bold">Content</div>
+                    <LNXLinkText href='/services' text='Services' className='block' />
+                    <LNXLinkText href='/process' text='Process' className='block' />
+                    <LNXLinkText href='/about' text='About' className='block' />
                 </div>
 
                 <div className="w-56">
                 </div>
+            </LNXRow >
 
-                <div className="w-56">
-                </div>
-            </LNXRow>
-
-            < div className='pt-8 text-xs text-theme-g-4' >
+            < div className='pt-8 text-xs text-theme-g-3' >
                 <p>Copyright &copy; 2023 Linux Consulting ApS</p>
             </div >
             < div className='pt-8 text-xs text-theme-g-4' >
