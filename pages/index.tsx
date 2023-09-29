9410f0b1f030048e1730b8eca4d92d9fc1421f20
Mogens Winther <mw@mowin.dk>
2023-09-29 15:23:04 +0200

    Updated text and article list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,17 @@
 import Layout from '../components/Layout';
-// import {
-//   getCMSHeaderData,
-//   getCMSFooterData,
-//   getCMSArticleByType,
-// } from '../libs/lib-cms/services';
-import {
-  CMSArticleView,
-} from '../libs/lib-cms/views';
-import { LNXHeaderSection, LNXImageGridCard, LNXLineDark, LNXMarkdownGridCard, LNXTextSection, LNXTwoGrid } from '../libs/lib-lnx/components';
+import { LNXHeaderSection, LNXTextSection, LNXTwoGrid } from '../libs/lib-lnx/components';
 import {
   isLNXStagingMode,
   getLNXRevalidationTime,
 } from '../libs/lib-lnx/utils';
 import { LNXRow, LNXCircleTextCard } from '../libs/lib-lnx/components';
-import ReactMarkdown from 'react-markdown';
 import { getPost } from '../services/PostsService';
 import { LNXBackgroundImageBlock, LNXMarkdownBlock } from '../libs/lib-lnx/components/Blocks';
 
 export async function getStaticProps() {
-  // const article = await getCMSArticleByType('index');
 
   return {
     props: {
-      // ...article,
-      // header_data: await getCMSHeaderData(),
-      // footer_data: await getCMSFooterData(),
       blocks: {
         chaos: await getPost('chaos', "posts/blocks"),
         empower: await getPost('empower', "posts/blocks"),
@@ -42,56 +29,54 @@ export async function getStaticProps() {
 export default function Home(props: any) {
 
   return (
-    <>
-      <Layout header_data={props.header_data} footer_data={props.footer_data} metadata={props.metadata}>
-        {props.draftMode && (
-          <div className="bg-alert fixed top-0 z-[1000] p-2 m-2 rounded">Staging - Cache expires in {props.revalidate} sec</div>
-        )}
-        <LNXHeaderSection>
-          <div>
-            <b>Need strong technical help?</b><br />
-            <span className='text-xl sm:text-2xl md:text-3xl'>Getting a good start, is half the job, and we can help with that!</span>
-          </div>
-        </LNXHeaderSection>
-
-        <LNXTextSection>
-          When we are busy with the everyday tasks and don&quot;t have a movement to stop and get some perspective, then it can be very hard to see how to move forward.
-        </LNXTextSection>
-
-
-        <LNXRow>
-          <LNXCircleTextCard image_src='/images/small/migration.jpg' header='Migration' link_text='Read more' link_href='/services/migration' />
-          <LNXCircleTextCard image_src='/images/small/optimization.jpg' header='Optimization' link_text='Read more' link_href='/services/optimization' />
-          <LNXCircleTextCard image_src='/images/small/perspective.jpg' header='Perspective' link_text='Read more' link_href='/services/perspective' />
-          <LNXCircleTextCard image_src='/images/small/design.jpg' header='Design' link_text='Read more' link_href='/services/design' />
-          <LNXCircleTextCard image_src='/images/small/strategy.jpg' header='Strategy' link_text='Read more' link_href='/services/strategy' />
-        </LNXRow>
-
-        <div className='mt-8 py-8 bg-theme-g-0'>
-          <LNXTwoGrid>
-            <LNXMarkdownBlock data={props.blocks.chaos} />
-            <LNXBackgroundImageBlock src='/images/large/chaos.jpg' />
-          </LNXTwoGrid>
-          <div className='h-8' />
-          <LNXTwoGrid>
-            <LNXBackgroundImageBlock src='/images/large/team.jpg' />
-            <LNXMarkdownBlock data={props.blocks.empower} />
-          </LNXTwoGrid>
-          <div className='h-8' />
-          <LNXTwoGrid>
-            <LNXMarkdownBlock data={props.blocks.direction} />
-            <LNXBackgroundImageBlock src='/images/large/perspective.jpg' />
-          </LNXTwoGrid>
+    <Layout header_data={props.header_data} footer_data={props.footer_data} metadata={props.metadata}>
+      {props.draftMode && (
+        <div className="bg-alert fixed top-0 z-[1000] p-2 m-2 rounded">Staging - Cache expires in {props.revalidate} sec</div>
+      )}
+      <LNXHeaderSection>
+        <div>
+          <b>Need a strong technical Managing Consultant?</b><br />
+          <span className='text-xl sm:text-2xl md:text-3xl'>Getting a good start is half the job, and we can help with that!</span>
         </div>
+      </LNXHeaderSection>
 
-        <div className='Slim mt-8'>
-          <LNXTwoGrid>
-            <LNXMarkdownBlock data={props.blocks.hybrid} />
-            <LNXMarkdownBlock data={props.blocks.get_in_touch} />
-          </LNXTwoGrid>
-        </div>
+      <LNXTextSection>
+        When we are busy with everyday tasks and don&quot;t have a movement to stop and get some perspective, then it can be very hard to see how to move forward.
+      </LNXTextSection>
 
-      </Layout>
-    </>
+
+      <LNXRow>
+        <LNXCircleTextCard image_src='/images/small/management.jpg' header='Management' link_text='Read more' link_href='/services/management' />
+        <LNXCircleTextCard image_src='/images/small/optimization.jpg' header='Optimization' link_text='Read more' link_href='/services/optimization' />
+        <LNXCircleTextCard image_src='/images/small/migration.jpg' header='Migration' link_text='Read more' link_href='/services/migration' />
+        <LNXCircleTextCard image_src='/images/small/perspective.jpg' header='Perspective' link_text='Read more' link_href='/services/perspective' />
+        <LNXCircleTextCard image_src='/images/small/strategy.jpg' header='Strategy' link_text='Read more' link_href='/services/strategy' />
+      </LNXRow>
+
+      <div className='mt-8 py-8 bg-theme-g-0'>
+        <LNXTwoGrid>
+          <LNXMarkdownBlock data={props.blocks.chaos} />
+          <LNXBackgroundImageBlock src='/images/large/chaos.jpg' />
+        </LNXTwoGrid>
+        <div className='h-8' />
+        <LNXTwoGrid>
+          <LNXBackgroundImageBlock src='/images/large/team.jpg' />
+          <LNXMarkdownBlock data={props.blocks.empower} />
+        </LNXTwoGrid>
+        <div className='h-8' />
+        <LNXTwoGrid>
+          <LNXMarkdownBlock data={props.blocks.direction} />
+          <LNXBackgroundImageBlock src='/images/large/perspective.jpg' />
+        </LNXTwoGrid>
+      </div>
+
+      <div className='Slim mt-8'>
+        <LNXTwoGrid>
+          <LNXMarkdownBlock data={props.blocks.hybrid} />
+          <LNXMarkdownBlock data={props.blocks.get_in_touch} />
+        </LNXTwoGrid>
+      </div>
+
+    </Layout>
   );
 }
