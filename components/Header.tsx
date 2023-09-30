12e6d3f99ac05a3e294d535a2c03d971b54c653e
Mogens Winther <mw@mowin.dk>
2023-09-30 08:27:04 +0200

    Fixed small device header issue

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,10 @@ import { LNXLinkText, LNXRow } from "../libs/lib-lnx/components";
 
 export default function Header({ ...props }) {
     return (
-        <div id='NavHeader' className={`Header fixed top-0 left-0 z-10 w-full bg-white shadow-lg flex justify-center h-[62px] items-stretch`}>
+        <div id='NavHeader' className={`Header fixed top-0 left-0 z-10 w-full bg-white shadow-lg flex justify-center h-[62px] items-stretch font-bold pt-4 md:px-8`}>
 
             {/* Row */}
-            <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full items-stretch justify-items-center justify-center`}>
+            <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-8 w-full items-stretch justify-items-center justify-center`}>
 
                 {/* Logo - Left */}
                 <div className="justify-left mr-auto hidden md:block">
@@ -20,7 +20,7 @@ export default function Header({ ...props }) {
                 </div>
 
                 {/* Menu - Center */}
-                <div className='w-full pt-1  text-theme text-sm text-center'>
+                <div className='w-full pt-1 text-theme text-sm text-center'>
                     <LNXLinkText className='mx-4 inline' href='/' text='Home' />
                     <LNXLinkText className='mx-4 inline' href='/services' text='Services' />
                     <LNXLinkText className='mx-4 inline' href='/process' text='Process' />
@@ -30,8 +30,10 @@ export default function Header({ ...props }) {
 
                 {/* Name - Right */}
                 <div className='text-theme-g-3 text-sm justify-end ml-auto hidden lg:block mt-[-6px] h-[40px]'>
-                    Linux Consulting ApS, Denmark<br />
-                    Managing Consultant, Mogens Winther
+                    <Link href='/get-in-touch'>
+                        Linux Consulting ApS, Denmark<br />
+                        Managing Consultant, Mogens Winther
+                    </Link>
                 </div>
             </div>
         </div>
