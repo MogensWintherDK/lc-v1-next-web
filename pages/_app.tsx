568b14409f7d98a710100c170f473b7e729034a6
Mogens Winther <mw@mowin.dk>
2023-10-03 12:25:58 +0200

    Added Google Analytics support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,30 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+// import { Analytics } from '@vercel/analytics/react';
+import { isLNXProductionMode } from '../libs/lib-lnx/utils/Env';
 import '../styles/global.scss';
-import { Analytics } from '@vercel/analytics/react';
+
+const isProd = isLNXProductionMode();
 
 export default function App({ Component, pageProps }: { Component: any, pageProps: any }) {
+    const router = useRouter();
+    useEffect(() => {
+        const handleRouteChange = (url: string) => {
+            if (isProd) {
+                window.gtag('config', process.env.LNX_GOOGLE_ANALYTICS_ID as string, {
+                    page_path: url,
+                });
+            }
+        }
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        }
+    }, [router.events]);
     return (
         <>
             <Component {...pageProps} />
-            <Analytics />
+            {/* <Analytics /> */}
         </>
     );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,38 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import { isLNXProductionMode } from '../libs/lib-lnx/utils/Env';
+
+const gtag = `https://www.googletagmanager.com/gtag/js?id=${process.env.LNX_GOOGLE_ANALYTICS_ID}`;
+const isProd = isLNXProductionMode();
 
 export default function Document() {
     return (
         <Html>
             <Head>
                 <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
+                {isProd && (
+                    <>
+                        <script async src={gtag}></script>
+                        {/* Google tag (gtag.js) */}
+                        <script
+                            dangerouslySetInnerHTML={{
+                                __html: `
+                                    window.dataLayer = window.dataLayer || [];
+                                    function gtag(){dataLayer.push(arguments);}
+                                    gtag('js', new Date());
+                                    gtag('config', '${process.env.LNX_GOOGLE_ANALYTICS_ID}', {
+                                    page_path: window.location.pathname
+                                    });
+                                `,
+                            }}
+                        />
+
+                    </>
+                )}
             </Head>
             <body>
                 <Main />
                 <NextScript />
             </body>
-        </Html>
+        </Html >
     )
 }
