fa6fb25241ca8615bca4744ff88aadd818aabcce
Mogens Winther <mw@mowin.dk>
2023-10-03 16:02:11 +0200

    Added https:// as default to LNX_HOSTNAME

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 // Default code you can customize according to your requirements.
 module.exports = {
-    siteUrl: process.env.LNX_HOSTNAME,
+    siteUrl: 'https://' + process.env.LNX_HOSTNAME,
     generateRobotsTxt: true,
     generateIndexSitemap: false,
 }
