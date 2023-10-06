c76a8296b4fd2433f90c55a6d673ea8663106d82
Mogens Winther <mw@mowin.dk>
2023-10-06 14:08:04 +0200

    Updated for new article blocks and images

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import {
   isLNXStagingMode,
   getLNXRevalidationTime,
 } from '../libs/lib-lnx/utils';
-import { LNXRow, LNXCircleTextCard } from '../libs/lib-lnx/components';
+import { LNXRow, LNXCircleTextCard, LNXSquareTextCard } from '../libs/lib-lnx/components';
 import { getPostBlock } from '../services/PostsService';
 import { LNXBackgroundImageBlock, LNXMarkdownBlock } from '../libs/lib-lnx/components/Blocks';
 import { getLNXFullUrl } from '../libs/lib-lnx/utils/Metadata';
@@ -15,9 +15,10 @@ export async function getStaticProps() {
   return {
     props: {
       blocks: {
-        chaos: await getPostBlock('chaos'),
-        empower: await getPostBlock('empower'),
+        intro: await getPostBlock('intro'),
         direction: await getPostBlock('direction'),
+        empower: await getPostBlock('empower'),
+        forward: await getPostBlock('forward'),
         hybrid: await getPostBlock('hybrid'),
         get_in_touch: await getPostBlock('get-in-touch'),
       },
@@ -26,7 +27,7 @@ export async function getStaticProps() {
       metadata: {
         title: 'IT Technical and Managing Consultant',
         description: 'Get experienced help to solve your challenges faster',
-        keywords: 'IT, Technical, Managing, Chaos, Experience, Developer, Empower, Team, Big picture, Direction',
+        keywords: 'IT, Technical, Managing, Experience, Developer, Empower, Team, Big picture, Direction',
         url: getLNXFullUrl('/'),
         thumb: getLNXFullUrl('/images/logo/LC-Logo-Circle-V2-300px.png'),
       } as ILNXMetadata,
@@ -56,27 +57,40 @@ export default function Home(props: any) {
 
       <LNXRow>
         <LNXCircleTextCard image_src='/images/small/management.jpg' header='Management' link_text='Read more' link_href='/services/management' />
-        <LNXCircleTextCard image_src='/images/small/optimization.jpg' header='Optimization' link_text='Read more' link_href='/services/optimization' />
+        <LNXCircleTextCard image_src='/images/small/optimization_code.jpg' header='Optimization' link_text='Read more' link_href='/services/optimization' />
         <LNXCircleTextCard image_src='/images/small/migration.jpg' header='Migration' link_text='Read more' link_href='/services/migration' />
         <LNXCircleTextCard image_src='/images/small/perspective.jpg' header='Perspective' link_text='Read more' link_href='/services/perspective' />
-        <LNXCircleTextCard image_src='/images/small/strategy.jpg' header='Strategy' link_text='Read more' link_href='/services/strategy' />
+        <LNXCircleTextCard image_src='/images/small/strategy_road.jpg' header='Strategy' link_text='Read more' link_href='/services/strategy' />
       </LNXRow>
 
       <div className='mt-8 py-8 bg-theme-g-0'>
         <LNXTwoGrid>
-          <LNXMarkdownBlock data={props.blocks.chaos} />
-          <LNXBackgroundImageBlock src='/images/large/chaos.jpg' />
+          <LNXMarkdownBlock data={props.blocks.intro} />
+          <LNXBackgroundImageBlock src='/images/large/ocean.jpg' />
         </LNXTwoGrid>
         <div className='h-8' />
         <LNXTwoGrid>
-          <LNXBackgroundImageBlock src='/images/large/team.jpg' />
-          <LNXMarkdownBlock data={props.blocks.empower} />
-        </LNXTwoGrid>
-        <div className='h-8' />
-        <LNXTwoGrid>
-          <LNXMarkdownBlock data={props.blocks.direction} />
           <LNXBackgroundImageBlock src='/images/large/perspective.jpg' />
+          <LNXMarkdownBlock data={props.blocks.direction} />
         </LNXTwoGrid>
+        <div className='h-8' />
+        <LNXTwoGrid>
+          <LNXMarkdownBlock data={props.blocks.empower} />
+          <LNXBackgroundImageBlock src='/images/large/team.jpg' />
+        </LNXTwoGrid>
+        <div className='h-8' />
+        <LNXTwoGrid>
+          <LNXBackgroundImageBlock src='/images/large/shore.jpg' />
+          <LNXMarkdownBlock data={props.blocks.forward} />
+        </LNXTwoGrid>
+      </div>
+
+      <div className='Slim mt-8'>
+        <LNXHeaderSection>Cases</LNXHeaderSection>
+        <LNXRow cols='2'>
+          <LNXSquareTextCard image_src='/images/external/cloudprinter.png' header='Cloudprinter.com' link_text='Read more' link_href='/cases/cloudprintercom' />
+          <LNXSquareTextCard image_src='/images/external/fotodk.png' header='Foto.dk' link_text='Read more' link_href='/cases/fotodk' />
+        </LNXRow>
       </div>
 
       <div className='Slim mt-8'>
@@ -85,7 +99,6 @@ export default function Home(props: any) {
           <LNXMarkdownBlock data={props.blocks.get_in_touch} />
         </LNXTwoGrid>
       </div>
-
     </Layout>
   );
 }
